112d3e6adbc7a04e7f32c7f9d1782f569d2c3468
doskapps <50601180+doskapps@users.noreply.github.com>
2022-07-22 20:59:40 -0300

    Mapa en patina Ubicacion

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -13,7 +13,7 @@ const Contact = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 w-full">
             <div className="text-center col-span-1 lg:grid-cols-span-2 mt-20 mb-18 img-bg">
-                <div className="px-4 sm:px-0 mt-14">
+                <div className="px-4 sm:px-0 mt-10">
                     <h3 className="
                         md:mt-28
                         md:my-18
@@ -27,7 +27,6 @@ const Contact = () => {
                     >Nuestra Ubicación</h3>
 
                     <h4 className="
-                        mb-1
                         text-3xl
                         md:text-5xl
                         text-white 
@@ -39,7 +38,7 @@ const Contact = () => {
 
                 </div>
             </div>
-            <div className="flex justify-center bg-white mt-20">
+            <div className="flex justify-center bg-white mt-12">
                 <div className="w-full">
                 <iframe title="mapa villa barbara" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3936.8203537132827!2d-75.3750155!3d9.3491599!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e59173638bd2337%3A0xab1d4dd13b932a!2sFinca%20Villa%20Barbara!5e0!3m2!1ses!2sar!4v1658533207998!5m2!1ses!2sar" width="600" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
