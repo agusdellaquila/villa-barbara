6173d1617a768be8ee5dc6f01d7a95b1d837eb32
doskapps <50601180+doskapps@users.noreply.github.com>
2022-07-24 01:44:28 -0300

    Tamaño Btn Wsp

--- src/components/BtnWsp/BtnWsp.js
+++ src/components/BtnWsp/BtnWsp.js
@@ -4,8 +4,8 @@ import './BtnWsp.css'
 
 const BtnWsp = () => {
     return (
-        <a href="https://api.whatsapp.com/send?phone=573147134225" className="btn-wsp fixed w-20 h-20 bg-green-500 hover:bg-white" target="_blank">
-            <FaWhatsapp className="mx-auto my-4" />
+        <a href="https://api.whatsapp.com/send?phone=573147134225" className="btn-wsp fixed w-14 h-14 md:w-20 md:h-20 bg-green-500 hover:bg-white" target="_blank">
+            <FaWhatsapp className="mx-auto my-1 md:my-4 w-8 md:w-14" />
         </a>
     )
 }
